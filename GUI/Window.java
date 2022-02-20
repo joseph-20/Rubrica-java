b3e9fbf5afbf207d9cc64ebad22d6a3dedcd1c76
Andrea Pinto <andpnace00@gmail.com>
2022-02-20 18:22:45 +0100

    Begin implementing UI [PART 2]

--- GUI/Window.java
+++ GUI/Window.java
@@ -7,4 +7,8 @@ public class Window{
     private JTabbedPane Tabs;
     private JSplitPane SplitPaneAreaPrivata;
     private JSplitPane SplitPaneContatti;
+    private JPanel ListaContatti;
+    private JPanel InfoContatti;
+    private JPanel ListaContattiPrivata;
+    private JPanel InfoContattiPrivata;
 }
