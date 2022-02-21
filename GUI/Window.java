f6e1737d92ce7769853f8c111b0014173330f291
Andrea Pinto <andpnace00@gmail.com>
2022-02-22 00:28:47 +0100

    System LookAndFeel update. Frame created.

--- GUI/Window.java
+++ GUI/Window.java
@@ -1,10 +1,11 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Window{
-    JPanel panel;
-
+    //Dichiarazioni
+    private JPanel panel;
     private JTabbedPane Tabs;
     private JSplitPane SplitPaneAreaPrivata;
     private JSplitPane SplitPaneContatti;
@@ -14,4 +15,29 @@ public class Window{
     private JPanel InfoContattiPrivata;
     private JButton aggiungiContattoButton;
     private JButton creaGruppoButton;
+
+
+    public static void main(String[] args){
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        } catch (InstantiationException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        } catch (IllegalAccessException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        JFrame frame = new JFrame("Rubrica");
+        frame.setContentPane(new Window().panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setMinimumSize(new Dimension(1000, 750));
+    }
 }
