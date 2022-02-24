2e3105549bbc28528b678caf7ef3590b8c17f233
Andrea Pinto <andpnace00@gmail.com>
2022-02-24 12:21:16 +0100

    Connessione al database stabilita
    Creazione primo RS.
    
    Creazione JList
    
    Implementazione DLM

--- GUI/Window.java
+++ GUI/Window.java
@@ -1,13 +1,14 @@
 package GUI;
 
 import Database.ConnessioneDatabase;
+import Model.Contatto;
 import org.postgresql.util.PSQLException;
 
 import javax.swing.*;
 import java.awt.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+
+import Controller.Controller;
 
 public class Window {
     //Dichiarazioni
@@ -24,35 +25,56 @@ public class Window {
     private JPanel PanelBottoni;
     private JPanel PanelBottoniPrivati;
     private JButton aggiungiContattoPrivatoButton;
+    private JList list1;
 
+    public Window() throws SQLException{
+        Controller c = new Controller();
+        run(c);
+    }
 
-    public static void run() {
+    private void run(Controller c) throws SQLException {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (ClassNotFoundException e1) {
-            // TODO Auto-generated catch block
             e1.printStackTrace();
         } catch (InstantiationException e1) {
-            // TODO Auto-generated catch block
             e1.printStackTrace();
         } catch (IllegalAccessException e1) {
-            // TODO Auto-generated catch block
             e1.printStackTrace();
         } catch (UnsupportedLookAndFeelException e1) {
-            // TODO Auto-generated catch block
             e1.printStackTrace();
         }
+
+        //Creazione finestra principale
         JFrame frame = new JFrame("Rubrica");
-        frame.setContentPane(new Window().panel);
+        frame.setContentPane(this.panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
         frame.setVisible(true);
         frame.setMinimumSize(new Dimension(1000, 750));
+
+        //Connessione al database
+        Connection con = c.getConnection();
+
+        //Creazione della JList per la finestra Contatti
+        ResultSet set;
+        Statement s;
+        s=con.createStatement();
+        set = s.executeQuery("SELECT *" +
+                "FROM CONTATTO");
+        DefaultListModel DLM = new DefaultListModel();
+        while(set.next()){
+            Contatto cont = new Contatto();
+            System.out.println(set.getString("Nome"));
+            cont.setContatto(set.getString("nome"), set.getString("cognome"), set.getString("foto"), set.getBoolean("security"));
+            DLM.addElement(cont.getNome()+" "+cont.getCognome());
+        }
+        list1.setModel(DLM);
     }
 
-    public static void main(String[] args) throws SQLException {
-        run();
-        ConnessioneDatabase conn = new ConnessioneDatabase();
+
+    public static void main(String[] args) throws SQLException{
+        new Window();
 
     }
+
 }
--- Model/Contatto.java
+++ Model/Contatto.java
@@ -1,34 +1,22 @@
 package Model;
 
 public class Contatto {
-    //POSSIBILE SOLUZIONE 1
-
-    //Dichiarazione variabili
     private String nome;
     private String cognome;
+    private String imgPath;
     private boolean security;
-    private String imagePath;
 
-    //Costruttore
-    public Contatto(String n,String c,boolean sec,String im){
-        nome = n;
-        cognome = c;
-        security=sec;
-        imagePath=im;
+
+    public void setContatto(String name,String surname,String img,boolean sec){
+        nome = name;
+        cognome = surname;
+        imgPath = img;
+        security = sec;
     }
-
-    //Getter
     public String getNome(){
         return nome;
     }
     public String getCognome(){
         return cognome;
     }
-    public boolean getSecurity(){
-        return security;
-    }
-    public String getImagePath(){
-        return imagePath;
-    }
-
 }
